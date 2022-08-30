129815870848f28cc39becd50c99074aa13f55eb
Moritz Hess <hess@imbi.uni-freiburg.de>
2022-08-30 16:48:44 +0200

    export of dichotomize

--- src/PatternExtractor.jl
+++ src/PatternExtractor.jl
@@ -2,7 +2,7 @@ module PatternExtractor
 
 import LogLinearModels
 
-export select_k_variables, annotatesamplesbypattern
+export select_k_variables, annotatesamplesbypattern, dichotomize
 include("stepwise.jl")
 
 end # module PatternExtractor
